96475492c7014d18e0c2cc51582c889d5bc08fdf
Phương Oanh Bùi <oanhbui@Phuongs-MacBook-Pro.local>
2022-06-01 17:29:08 -0700

    Secure Store

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -35,7 +35,9 @@ function RenderCampsite(props) {
 
   const view = React.createRef();
 
-  const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+  const recognizeDrag = ({dx}) => (dx < -200 ? true : false);
+
+  const recognizeComment = ({dx}) => (dx > 200 ? true : false);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
@@ -63,6 +65,8 @@ function RenderCampsite(props) {
           ],
           { cancelable: false}
         );
+      } else if (recognizeComment(gestureState)) {
+        props.onShowModal();
       }
       return true;
     }
